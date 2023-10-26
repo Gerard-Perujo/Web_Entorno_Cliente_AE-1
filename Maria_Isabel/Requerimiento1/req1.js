9ac14b0cdc892df1aae073c5e548ce90e8994ffe
María Isabel <contacto.mims@gmail.com>
2023-10-26 16:27:57 +0200

    radio button terminado

--- Maria_Isabel/Requerimiento1/req1.js
+++ Maria_Isabel/Requerimiento1/req1.js
@@ -19,7 +19,7 @@ window.addEventListener("load",inicio);
         //Se crea la etiqueta/label del dni:
         var etiquetaDni=document.createElement("label")
         //Se crea el texto que precede a la caja de texto del dni
-        var cajaDni=document.createTextNode("DNI:");
+        var cajaDni=document.createTextNode("DNI: ");
         //A la variable etiqueta del dni se le añade este texto
         etiquetaDni.appendChild(cajaDni);
         //Se crea el elemento/campo input del dni
@@ -36,7 +36,7 @@ window.addEventListener("load",inicio);
         //Se crea la etiqueta/label del nombre:
         var etiquetaNombre=document.createElement("label")
         //Se crea el texto que precede a la caja de texto del nombre y apellidos
-        var cajaNombre=document.createTextNode("Nombre y apellidos:");
+        var cajaNombre=document.createTextNode("Nombre y apellidos: ");
         //A la variable etiqueta del nombre y apellidos se le añade este texto
         etiquetaNombre.appendChild(cajaNombre);
         //Se crea el elemento/campo input del nombre y los apellidos
@@ -47,13 +47,13 @@ window.addEventListener("load",inicio);
         campoNombre.name="nombre"
 
         //Salto de línea para añadir nueva etiqueta/label
-        var br2=document.createElement("br");
+        var br2=document.createElement("br"); 
 
     //--------------------------------------------Dirección------------------------------------------------
         //Se crea la etiqueta/label de la dirección:
         var etiquetaDireccion=document.createElement("label")
         //Se crea el texto que precede a la caja de texto de la dirección
-        var cajaDirección=document.createTextNode("Dirección:");
+        var cajaDirección=document.createTextNode("Dirección: ");
         //A la variable etiqueta de la dirección se le añade este texto
         etiquetaDireccion.appendChild(cajaDirección);
         //Se crea el elemento/campo input de la dirección
@@ -70,7 +70,7 @@ window.addEventListener("load",inicio);
         //Se crea la etiqueta/label del telefono:
         var etiquetaTelefono=document.createElement("label")
         //Se crea el texto que precede a la caja de texto del teléfono
-        var cajaTelefono=document.createTextNode("Teléfono:");
+        var cajaTelefono=document.createTextNode("Teléfono: ");
         //A la variable etiqueta del teléfono se le añade este texto
         etiquetaTelefono.appendChild(cajaTelefono);
         //Se crea el elemento/campo input del teléfono
@@ -81,30 +81,222 @@ window.addEventListener("load",inicio);
         campoTelefono.name="telefono"
 
         //Salto de línea
-        var br4=document.createElement("br");
+        var br4=document.createElement("br"); 
     
     //-------------------------------------------------------------------------------------------------------
         //Al fieldset 1 se le añaden los elementos anteriores 
         campo1.appendChild(leyenda);
+
         campo1.appendChild(etiquetaDni);
         campo1.appendChild(campoDni);
         campo1.appendChild(br1);
+
         campo1.appendChild(etiquetaNombre);
         campo1.appendChild(campoNombre);
         campo1.appendChild(br2);
+
         campo1.appendChild(etiquetaDireccion);
         campo1.appendChild(campoDireccion);
         campo1.appendChild(br3);
+
         campo1.appendChild(etiquetaTelefono);
         campo1.appendChild(campoTelefono);
         campo1.appendChild(br4);
-       
+    
+    //-------------------------------------------- Creación fielset 2 -------------------------------------------
+        var campo2=document.createElement("fieldset");
+        var leyenda2=document.createElement("legend");
+        //Se añade contenido a leyenda
+        var contenidoLeyenda2=document.createTextNode("¡Diseñe sus vacaciones!")
+        //Se añade el contenido de la leyenda a la variable leyenda
+        leyenda2.appendChild(contenidoLeyenda2);
+    
+    //--------------------------------------------Primer radio button-------------------------------------------------------
+        //Se crea la etiqueta/label del radio button:
+        var etiquetaRadio1=document.createElement("label")
+        //Se crea el texto que indica qué es lo que hay que seleccionar entre los siguientes radio buttons
+        var textoRadio1=document.createTextNode("Escoja un resort de parques fuera de Walt Disney World:");
+        //A la variable etiqueta de la primera tanda de radio buttons se le añade este texto
+        etiquetaRadio1.appendChild(textoRadio1);
+
+        var br5=document.createElement("br");
+
+        //-----Se crean los elementos/campos input seleccionables como radio button, incluyendo sus respectivos atributos-----
+        //------------------------------------------Universal------------------------------------------------------
+        var campoUniversal=document.createElement("input");
+        var nombreUniversal=document.createTextNode("Universal Orlando Resort");
+        campoUniversal.appendChild(nombreUniversal);
+        //Se añaden los siguientes atributos:
+        campoUniversal.type="radio"
+        campoUniversal.id="universal"
+        campoUniversal.name="alrededores"
+
+        //Salto de línea para añadir nuevo radio button
+        var br6=document.createElement("br");
+
+        //------------------------------------------Sea World------------------------------------------------------
+        var campoSea=document.createElement("input");
+        var nombreSea=document.createTextNode("SeaWorld Orlando");
+        campoSea.appendChild(nombreSea);
+        //Se añaden los siguientes atributos:
+        campoSea.type="radio"
+        campoSea.id="sea"
+        campoSea.name="alrededores"
+
+        //Salto de línea para añadir nuevo radio button
+        var br7=document.createElement("br");
+
+        //------------------------------------------Busch Gardens------------------------------------------------------
+        var campoBusch=document.createElement("input");
+        var nombreBusch=document.createTextNode("Busch Gardens Tampa Bay");
+        campoBusch.appendChild(nombreBusch);
+        //Se añaden los siguientes atributos:
+        campoBusch.type="radio"
+        campoBusch.id="busch"
+        campoBusch.name="alrededores"
+
+        //Salto de línea para añadir nuevo radio button
+        var br8=document.createElement("br");
+
+        //------------------------------------------Gatorland------------------------------------------------------
+        var campoGator=document.createElement("input");
+        var nombreGator=document.createTextNode("Gatorland");
+        campoGator.appendChild(nombreGator);
+        //Se añaden los siguientes atributos:
+        campoGator.type="radio"
+        campoGator.id="gator"
+        campoGator.name="alrededores"
+
+        //Salto de línea para añadir nueva tanda de radio buttons
+        var br9=document.createElement("br");
+        var br10=document.createElement("br");
+
+    //--------------------------------------------Segundo radio button-------------------------------------------------------
+        //Se crea la etiqueta/label del radio button 2:
+        var etiquetaRadio2=document.createElement("label")
+        //Se crea el texto que indica qué es lo que hay que seleccionar entre los siguientes radio buttons
+        var textoRadio2=document.createTextNode("Escoja una actividad fuera de los parques:");
+        //A la variable etiqueta de la primera tanda de radio buttons se le añade este texto
+        etiquetaRadio2.appendChild(textoRadio2);
+
+        var br11=document.createElement("br");
+
+        //-----Se crean los elementos/campos input seleccionables como radio button, incluyendo sus respectivos atributos-----
+        //------------------------------------------Visita------------------------------------------------------
+        var campoVisita=document.createElement("input");
+        var nombreVisita=document.createTextNode("Visita al downtown Orlando");
+        campoVisita.appendChild(nombreVisita);
+        //Se añaden los siguientes atributos:
+        campoVisita.type="radio"
+        campoVisita.id="visita"
+        campoVisita.name="actividad"
+
+        //Salto de línea para añadir nuevo radio button
+        var br12=document.createElement("br");
+
+        //------------------------------------------Outlet------------------------------------------------------
+        var campoOutlet=document.createElement("input");
+        var nombreOutlet=document.createTextNode("Comprar en los outlets");
+        campoOutlet.appendChild(nombreOutlet);
+        //Se añaden los siguientes atributos:
+        campoOutlet.type="radio"
+        campoOutlet.id="outlet"
+        campoOutlet.name="actividad"
+
+        //Salto de línea para añadir nuevo radio button
+        var br13=document.createElement("br"); 
+
+        //------------------------------------------Tren------------------------------------------------------
+        var campoTren=document.createElement("input");
+        var nombreTren=document.createTextNode("Coger el nuevo tren hacia Miami");
+        campoTren.appendChild(nombreTren);
+        //Se añaden los siguientes atributos:
+        campoTren.type="radio"
+        campoTren.id="tren"
+        campoTren.name="actividad"
+
+        //Salto de línea para añadir nuevo radio button
+        var br14=document.createElement("br");
+
+        //------------------------------------------Nadar------------------------------------------------------
+        var campoNadar=document.createElement("input");
+        var nombreNadar=document.createTextNode("Nadar con manatíes en Crystal River");
+        campoNadar.appendChild(nombreNadar);
+        //Se añaden los siguientes atributos:
+        campoNadar.type="radio"
+        campoNadar.id="nadar"
+        campoNadar.name="actividad"
+
+        //Salto de línea para añadir nueva tanda de checkboxes
+        var br15=document.createElement("br");
+
+    
+
+
+    //-------------------------------------------------------------------------------------------------------
+        //Al fieldset 2 se le añaden los elementos anteriores 
+        campo2.appendChild(leyenda2);
+        campo2.appendChild(etiquetaRadio1);
+        campo2.appendChild(br5);
+
+        campo2.appendChild(campoUniversal);
+        campo2.appendChild(nombreUniversal);
+        campo2.appendChild(br6);
+   
+        campo2.appendChild(campoSea);
+        campo2.appendChild(nombreSea);
+        campo2.appendChild(br7);
+
+        campo2.appendChild(campoBusch);
+        campo2.appendChild(nombreBusch);
+        campo2.appendChild(br8);
+
+        campo2.appendChild(campoGator);
+        campo2.appendChild(nombreGator);
+        campo2.appendChild(br9);
+        campo2.appendChild(br10);
+
+
+        campo2.appendChild(etiquetaRadio2);
+        campo2.appendChild(br11);
+
+        campo2.appendChild(campoVisita);
+        campo2.appendChild(nombreVisita);
+        campo2.appendChild(br12);
+
+        campo2.appendChild(campoOutlet) ;
+        campo2.appendChild(nombreOutlet);
+        campo2.appendChild(br13);
+
+        campo2.appendChild(campoTren);
+        campo2.appendChild(nombreTren);
+        campo2.appendChild(br14);
+
+        campo2.appendChild(campoNadar);
+        campo2.appendChild(nombreNadar);
+        campo2.appendChild(br15);
+
+  
+
+
+
+
+    //-------------------------------------------- Final -------------------------------------------
         //Al formulario se le añade el fieldset 1
         formulario.appendChild(campo1);
+
+        //Separamos los fielset
+        var br16=document.createElement("br");
+        formulario.appendChild(br16);
+
+        //Al formulario se le añade el fieldset 2
+        formulario.appendChild(campo2);
+        
         //Al div se le añade el formulario
         divi.appendChild(formulario);
+        
         //Al body se le añade el div
-        document.body.appendChild(divi); 
+        document.body.appendChild(divi);  
 
     }
     
