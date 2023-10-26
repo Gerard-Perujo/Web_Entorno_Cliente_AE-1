0da350226b4753aef39f30170d69691854761cb0
María Isabel <contacto.mims@gmail.com>
2023-10-26 13:44:21 +0200

    js arranca

--- Maria_Isabel/Requerimiento1/req1.js
+++ Maria_Isabel/Requerimiento1/req1.js
@@ -1,48 +1,43 @@
-window.addEventListener("load", function inicio(){
-    
-    loading();
+//Para cargar las funciones se añade el siguiente escuchador, es decir, cuando cargue la página se carga también lo que hay
+//dentro de la función inicio.
+window.addEventListener("load",inicio);
 
-    function loading(){
-        /*
-        //Crear nodo formulario
-        let formulario=document.createElement("form");
-        formulario.method="get";    
-        //formulario.id="jsFormulario";   
-        document.body.appendChild(formulario); 
-
-        //Crear nodo fieldset
-        let field1=document.createElement("fieldset");   
-        document.body.form.appendChild(field1);  
-
-        
-        //Crear nodo legend
-        let leyenda1=document.createElement("legend");
-        let leyendita1=document.createTextNode("Por favor, rellene los  siguientes datos personales:");
-        leyenda1.appendChild(leyendita1);
-        document.body.form.fieldset.appendChild("leyenda1");
-        */
-
-        let formulario=document.createElement("form")
-        formulario.setAttribute("method", "get")
-        formulario.setAttribute("id", "jsFormulario")
-
-        let field=document.createElement("fieldset")
-        
+    //Dentro de la función inicio se crea todo el DOM del ejercicio:
+    function inicio() {
+        var divi= document.createElement("div");
+       var formulario =document.createElement("form");
+       var campo=document.createElement("fieldset");
+       var leyenda=document.createElement("legend");
+       var etiqueta=document.createElement("label")
+       //se añade contenido a leyenda
+       var contenidoLeyenda=document.createTextNode("Por favor rellene los siguientes datos personales: ")
+      //se añade contenido a la variable leyenda
+      leyenda.appendChild(contenidoLeyenda);
+       //se añade cotenido a label
+       var contenidoLabel=document.createTextNode("DNI");
+       //el contenido se añade a la variable etiqueta
+       etiqueta.appendChild(contenidoLabel);
+       //campo input
+       var campoDni=document.createElement("input");
+       //atributos de campoDni
+       campoDni.type="text"
+       campoDni.id="dni"
+       campoDni.name="DNI"
 
 
-        div1.appendChild(formulario)
-        
-
-           
-
-        
+      //se añade al fieldset 1
+      campo.appendChild(leyenda);
+        campo.appendChild(etiqueta);
+       campo.appendChild(campoDni);
+       
+       //SE AÑADE AL FORMULARIO
+       
+       formulario.appendChild(campo);
+       
+      //se añade al div
+        divi.appendChild(formulario);
+       //SE AÑADE AL BODY
+       document.body.appendChild(divi); 
 
     }
-       
     
-    })
-
-
-
-
-
